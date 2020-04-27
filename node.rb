8f7d37809c0ce40081a90bcca1cbece80a4762f4
Nick <nsardonini@gmail.com>
2020-04-27 15:12:14 -0400

    define Node class
    constructor sets value and left & right children to nil by default.
    included Comparable module to mixin comparison methods for setting child values.

---
+++ node.rb
@@ -0,0 +1,14 @@
+class Node
+  include Comparable
+  attr_accessor :value, :left_child, :right_child
+
+  def initialize value = nil, left_child = nil, right_child = nil
+    @value       = value
+    @left_child  = left_child
+    @right_child = right_child
+  end
+
+  def <=> node 
+    self.value <=> node.value
+  end
+end
