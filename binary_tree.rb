8df0f51bc1268478886e0f0d77ece27a6a68cbdd
Nick <nsardonini@gmail.com>
2020-04-28 14:47:04 -0400

    remove comments

--- binary_tree.rb
+++ binary_tree.rb
@@ -29,17 +29,6 @@ class Tree
           end
         end
       end
-      # until node.left_child == item || node.right_child == item
-      #   comparison = item <=> node
-      #   if comparison == -1 && !node.left_child
-      #     node.left_child = item
-      #   elsif comparison == 1 && !node.right_child
-      #     node.right_child = item
-      #   else
-      #     node = node.left_child  if comparison == -1
-      #     node = node.right_child if comparison == 1
-      #   end
-      # end
     end
     root
   end
