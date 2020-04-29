4af1a1207163a81f4fff79da1d954549cb1bc071
Nick <nsardonini@gmail.com>
2020-04-29 19:58:14 -0400

    add parent instance variable to Node. remove #==

--- node.rb
+++ node.rb
@@ -1,18 +1,15 @@
 class Node
   include Comparable
-  attr_accessor :value, :left_child, :right_child
+  attr_accessor :value, :left_child, :right_child, :parent
 
-  def initialize value = nil, left_child = nil, right_child = nil
+  def initialize value = nil, left_child = nil, right_child = nil, parent = nil
     @value       = value
     @left_child  = left_child
     @right_child = right_child
+    @parent      = parent
   end
 
   def <=> node 
     self.value <=> node.value
-  end
-
-  def == other
-    self == other
   end
 end
