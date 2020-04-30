f699eb358f5603b348da2ef9dbe755ac554762c8
Nick <nsardonini@gmail.com>
2020-04-30 19:45:52 -0400

    move parent parameter for easier access when calling method

--- node.rb
+++ node.rb
@@ -2,7 +2,7 @@ class Node
   include Comparable
   attr_accessor :value, :left_child, :right_child, :parent
 
-  def initialize value = nil, left_child = nil, right_child = nil, parent = nil
+  def initialize value = nil, parent = nil, left_child = nil, right_child = nil
     @value       = value
     @left_child  = left_child
     @right_child = right_child
