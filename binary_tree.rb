6168a3108a3191233812455eefe365b8bfee5650
Nick <nsardonini@gmail.com>
2020-05-02 15:14:33 -0400

    change left_child, right_child to left, right. refactor #delete
    condensed control flow for the cases of no children and one child.
    added return statements to prevent rest of method from executing if those
    cases were truthy

--- binary_tree.rb
+++ binary_tree.rb
@@ -15,14 +15,14 @@ class Tree
     data.each_with_index do |item, index|
       next if index == 0
       node = root
-      until node.left_child == item || node.right_child == item
+      until node.left == item || node.right == item
         case (item <=> node)
         when -1 # Move left
-          node.left_child.nil? ? node.left_child = item : node = node.left_child
-          node.left_child.parent = node if node.left_child == item
+          node.left.nil? ? node.left = item : node = node.left
+          node.left.parent = node if node.left == item
         when 1 # Move right
-          node.right_child.nil? ? node.right_child = item : node = node.right_child
-          node.right_child.parent = node  if node.right_child == item
+          node.right.nil? ? node.right = item : node = node.right
+          node.right.parent = node  if node.right == item
         end
       end
     end
@@ -31,9 +31,9 @@ class Tree
 
   def insert value, node = @root
     if (value < node.value) # Move left
-      node.left_child.nil? ? node.left_child = Node.new(value, node) : insert(value, node.left_child)
+      node.left.nil? ? node.left = Node.new(value, node) : insert(value, node.left)
     elsif (value > node.value) # Move right
-      node.right_child.nil? ? node.right_child = Node.new(value, node) : insert(value, node.right_child)
+      node.right.nil? ? node.right = Node.new(value, node) : insert(value, node.right)
     end
     @data << value unless @data[-1] == value
   end
@@ -44,64 +44,60 @@ class Tree
     parent = node.parent
 
     # no children
-    if node.left_child.nil? && node.right_child.nil?
-      parent.left_child = nil if parent.left_child  == node
-      parent.left_child = nil if parent.right_child == node
+    if node.left.nil? && node.right.nil?
+      parent.left  == node ? parent.left = nil : parent.right = nil
       node.parent = nil
-      return @root
+      return root
     end
     # one child
-    if parent.left_child == node
-      parent.left_child = node.left_child  if node.left_child && node.right_child.nil?
-      parent.left_child = node.right_child if node.right_child && node.left_child.nil?
-      node.left_child.parent =  parent.left_child if node.left_child
-      node.right_child.parent = parent.left_child if node.right_child
-    else
-      parent.right_child = node.left_child  if node.left_child && node.right_child.nil?
-      parent.right_child = node.right_child if node.right_child && node.left_child.nil?
-      node.left_child.parent  = parent.right_child if node.left_child
-      node.right_child.parent = parent.right_child if node.right_child
+    if node.left && node.right.nil?
+      parent.left == node ? parent.left = node.left : parent.right = node.left
+      node.left.parent = node.parent
+      return root
+    elsif node.right && node.left.nil?  
+      parent.left == node ? parent.left = node.right : parent.right = node.right
+      node.right.parent = node.parent
+      return root
     end
     # two children
-    if node.left_child && node.right_child
+    if node.left && node.right
       # in-order successor --- right child's left most child
-      replacement = node.right_child
-      replacement = replacement.left_child while replacement.left_child
-
+      replacement = node.right
+      replacement = replacement.left while replacement.left
       # covers case where the in-order successor     eg. delete 50;  50
       # is a child of the node to be deleted                           \
       #                                    successor has no children -  70
-      if replacement == node.right_child
+      if replacement == node.right
         replacement.parent = node.parent
-        replacement.left_child = node.left_child
-        node.left_child.parent = replacement
-        node.parent.left_child  = replacement if node.parent.left_child  == node
-        node.parent.right_child = replacement if node.parent.right_child == node
-        node.left_child, node.right_child, node.parent = nil, nil, nil
-        return @root
+        replacement.left   = node.left
+        node.left.parent   = replacement
+        node.parent.left   = replacement if node.parent.left  == node
+        node.parent.right  = replacement if node.parent.right == node
+        node.left, node.right, node.parent = nil, nil, nil
+        return root
       end
       # case where replacement node has no children
-      replacement.right_child.parent = replacement.parent unless replacement.right_child.nil?
-      replacement.right_child.nil? ? replacement.parent.left_child = nil : replacement.parent.left_child = replacement.right_child
+      replacement.right.parent = replacement.parent unless replacement.right.nil?
+      replacement.right.nil? ? replacement.parent.left = nil : replacement.parent.left = replacement.right
 
       # change replacement's children & parent nodes to node's children and parent nodes
       # accounts for the case of the replacement being a child of the removed node
-      replacement.right_child = node.right_child
-      replacement.left_child  = node.left_child
-      replacement.parent      = node.parent
-      # change node's children to point at replacement as a parent
-      node.left_child.parent  = replacement
-      node.right_child.parent = replacement 
+      replacement.right  = node.right
+      replacement.left   = node.left
+      replacement.parent = node.parent
+      # change node's children to point to replacement as a parent
+      node.left.parent  = replacement
+      node.right.parent = replacement 
       # change node's parent to point to replacement
-      node.parent.left_child  = replacement if parent.left_child  == node
-      node.parent.right_child = replacement if parent.right_child == node
+      node.parent.left  = replacement if node.parent.left  == node
+      node.parent.right = replacement if node.parent.right == node
     end   
-    @root
+    root
   end
 
   def find value, node = @root
     until node.value == value
-      value < node.value ? node = node.left_child : node = node.right_child
+      value < node.value ? node = node.left : node = node.right
       return nil if node.nil?
     end
     node
