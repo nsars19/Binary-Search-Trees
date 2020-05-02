61e5f78f8c8e3bdcdbeda9daed93d88b3e2b6ca4
Nick <nsardonini@gmail.com>
2020-05-02 15:14:24 -0400

    change left_child, right_child to left, right

--- node.rb
+++ node.rb
@@ -1,12 +1,12 @@
 class Node
   include Comparable
-  attr_accessor :value, :left_child, :right_child, :parent
+  attr_accessor :value, :left, :right, :parent
 
-  def initialize value = nil, parent = nil, left_child = nil, right_child = nil
-    @value       = value
-    @left_child  = left_child
-    @right_child = right_child
-    @parent      = parent
+  def initialize value = nil, parent = nil, left = nil, right = nil
+    @value  = value
+    @left   = left
+    @right  = right
+    @parent = parent
   end
 
   def <=> node 
